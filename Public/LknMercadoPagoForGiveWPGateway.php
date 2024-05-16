e808a892d2ecb8820f397ac10b31507428550af0
CaioAndersonMM <caioandersonmartins@gmail.com>
2024-05-16 19:32:08 +0000

    update: Deixa o pagamento pendente assim que realiza doação.

--- Public/LknMercadoPagoForGiveWPGateway.php
+++ Public/LknMercadoPagoForGiveWPGateway.php
@@ -8,6 +8,7 @@ use Give\Donations\ValueObjects\DonationStatus;
 use Give\Framework\Exceptions\Primitives\Exception;
 use Give\Framework\PaymentGateways\Commands\GatewayCommand;
 use Give\Framework\PaymentGateways\Commands\PaymentComplete;
+use Give\Framework\PaymentGateways\Commands\PaymentPending;
 use Give\Framework\PaymentGateways\Commands\PaymentRefunded;
 use Give\Framework\PaymentGateways\Exceptions\PaymentGatewayException;
 use Give\Framework\PaymentGateways\PaymentGateway;
@@ -59,16 +60,18 @@ final class LknMercadoPagoForGiveWPGateway extends PaymentGateway {
     public function createPayment(Donation $donation, $gatewayData): GatewayCommand {
         try {
             // Step 1: Validate any data passed from the gateway fields in $gatewayData.  Throw the PaymentGatewayException if the data is invalid.
-            if (empty($gatewayData['example-gateway-id'])) {
-                throw new PaymentGatewayException(__('Example payment ID is required.', 'example-give' ) );
-            }
+            // if (empty($gatewayData['example-gateway-id'])) {
+            //     throw new PaymentGatewayException(__('Example payment ID is required.', 'example-give' ) );
+            // }
 
-            // Step 2: Create a payment with your gateway.
-            $response = $this->exampleRequest(array('transaction_id' => $gatewayData['example-gateway-id']));
+            // // Step 2: Create a payment with your gateway.
+            // $response = $this->exampleRequest(array('transaction_id' => $gatewayData['example-gateway-id']));
 
-            // Step 3: Return a command to complete the donation. You can alternatively return PaymentProcessing for gateways that require a webhook or similar to confirm that the payment is complete. PaymentProcessing will trigger a Payment Processing email notification, configurable in the settings.
+            // // Step 3: Return a command to complete the donation. You can alternatively return PaymentProcessing for gateways that require a webhook or similar to confirm that the payment is complete. PaymentProcessing will trigger a Payment Processing email notification, configurable in the settings.
             
-            return new PaymentComplete($response['transaction_id']);
+            // return new PaymentComplete($response['transaction_id']);
+
+            return new PaymentPending();
         } catch (Exception $e) {
             // Step 4: If an error occurs, you can update the donation status to something appropriate like failed, and finally throw the PaymentGatewayException for the framework to catch the message.
             $errorMessage = $e->getMessage();
@@ -132,8 +135,8 @@ final class LknMercadoPagoForGiveWPGateway extends PaymentGateway {
         $urlsPreferences = array(
             'back_urls' => array(
                 'success' => $url_pagina_personalizada,
-                'pending' => $url_pagina_personalizada . '&status=pending',
-                'failure' => $url_pagina_personalizada . '&status=failure'
+                'pending' => $url_pagina_personalizada,
+                'failure' => $url_pagina_personalizada
             )
         );
         wp_localize_script(self::id(), 'preferencia', $urlsPreferences);
